845fb61568c5df709b55c8243d03046cde635b89
zeeshandev6 <zeeshan.dev6@gmail.com>
2019-03-31 21:48:41 +0500

    Database Migrations

--- database/migrations/2019_03_29_160007_create_user_group_table.php
+++ database/migrations/2019_03_29_160007_create_user_group_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserGroupTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_group', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('userimage')->nullable();
-            $table->string('api_key')->nullable()->unique();
-            $table->rememberToken();
+            $table->string('group_name');
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_group');
     }
 }
